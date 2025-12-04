821dc9f80958f22c3612c556825b487962ecaff3
gpcprado <gpcprado@gbox.ncf.edu.ph>
2025-12-04 22:43:34 +0800

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -7,8 +7,12 @@ import 'reflect-metadata';
 async function bootstrap() {
   dotenv.config();
   const app = await NestFactory.create(AppModule);
-  const port = process.env.PORT || 3001;
+  const port = process.env.PORT || 3000;
   app.enableCors({
+    origin: [
+      "http://localhost:3000",
+      "https://192.168.10.109:3000"
+      ]
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
